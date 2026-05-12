39f02f3e17b96b2254deb9914b5c0cf953d8fe04
Smriti Singh <Smritisingh9180@gmail.com>
2026-05-12 23:13:37 +0530

    debug: log MongoDB URI

--- hrm/hrm-backend/src/config/db.js
+++ hrm/hrm-backend/src/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+   console.log('URI:', process.env.MONGODB_URI);
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'hrms',
